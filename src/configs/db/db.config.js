d08da9778a83be94fd32f649eabfc1cbacd86cb7
mtri-git <vmtri20@gmail.com>
2023-03-10 22:08:12 +0700

    update

--- src/configs/db/db.config.js
+++ src/configs/db/db.config.js
@@ -4,6 +4,7 @@ require('dotenv/config')
 async function connect(){
     try{
         console.log('Connecting to database...')
+        mongoose.set("strictQuery", false)
         await mongoose.connect(process.env.DB_ONLINE)
         console.log('Connect to database successfully!')
     }
