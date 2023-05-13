c0437d0110a97d3fad962855204feb6270a65609
mtri-git <vmtri20@gmail.com>
2023-05-13 09:25:39 +0700

    update

--- src/configs/socket2.config.js
+++ src/configs/socket2.config.js
@@ -1,68 +1,74 @@
-const socketIo = require('socket.io');
-const Message = require('../models/Message');
-const Room = require('../models/Room');
-let io;
+const socketIo = require('socket.io')
+const Message = require('../models/Message')
+const Room = require('../models/Room')
+let io
 
 function getIo() {
-  return io;
+	return io
 }
 
 function initSocket(server) {
-  io = socketIo(server);
-  
-  io.on('connection', (socket) => {
-    console.log('A user connected.');
-    socket.on('join_room', (data) => {
-      const { username, rooms } = data;
-      console.log(rooms);
-      rooms.forEach((room) => {
-        console.log(`${username} joined room ${room}`);
-        socket.join(room);
-      });
-    });
+	io = socketIo(server)
 
-    
-    socket.on('send_message', async(data) => {
-      console.log(data);
-      const { roomId, userId, message, nanoid } = data;
-      io.to(roomId).emit('receive_message', data);
-      const messageData = {nanoid, roomId, content: message, sender: userId}
+	io.on('connection', (socket) => {
+		console.log('A user connected.')
+		socket.on('join_room', (data) => {
+			const { username, rooms } = data
+			console.log(rooms)
+			rooms.forEach((room) => {
+				console.log(`${username} joined room ${room}`)
+				socket.join(room)
+			})
+		})
 
-      // save message to db
-      const _message = new Message({
-        ...messageData,
-        reaction: [],
-        seen_by: [],
-        deleted_at: null,
-      })
-      await _message.save()
-      await Room.updateOne(
-        { _id: message.roomId },
-        { $set: { last_message: message._id } }
-      )
-    });
+		socket.on('send_message', async (data) => {
+			console.log(data)
+			const { roomId, userId, message, nanoid } = data
+			io.to(roomId).emit('receive_message', data)
+			const messageData = {
+				nanoid,
+				roomId,
+				content: message,
+				sender: userId,
+			}
 
-    socket.on('typing', (data) => {
-      console.log('typing')
-      const { roomId } = data;
-      io.to(roomId).emit('typing', data);
-    });
+			// save message to db
+			if ((type = 'text')) {
+				const _message = new Message({
+					...messageData,
+					reaction: [],
+					seen_by: [],
+					deleted_at: null,
+				})
+				await _message.save()
+				await Room.updateOne(
+					{ _id: message.roomId },
+					{ $set: { last_message: message._id } }
+				)
+			}
+		})
 
-    socket.on('stop_typing', (data) => {
-      console.log('stop typing')
-      const { roomId } = data;
-      io.to(roomId).emit('stop_typing', data);
-    });
+		socket.on('typing', (data) => {
+			console.log('typing')
+			const { roomId } = data
+			io.to(roomId).emit('typing', data)
+		})
 
-    socket.on('delete_message', (data) => {
-      const { roomId } = data;
-      io.in(roomId).emit('recall_message', data);
-    });
-    socket.on('disconnect', () => {});
-  });      
+		socket.on('stop_typing', (data) => {
+			console.log('stop typing')
+			const { roomId } = data
+			io.to(roomId).emit('stop_typing', data)
+		})
+
+		socket.on('delete_message', (data) => {
+			const { roomId } = data
+			io.in(roomId).emit('recall_message', data)
+		})
+		socket.on('disconnect', () => {})
+	})
 }
 
 module.exports = {
-  initSocket,
-  getIo,
-};
+	initSocket,
+	getIo,
+}
