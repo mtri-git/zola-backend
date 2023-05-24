7ffbaa4fcb7f83ce10fc5851d76762de1662d108
mtri-git <vmtri20@gmail.com>
2023-05-24 18:23:56 +0700

    update

--- src/configs/socket.config.js
+++ src/configs/socket.config.js
@@ -73,12 +73,16 @@ function initSocket(server) {
 						{ $set: { last_message: message._id } }
 					)
 				}
-				await sendPushNotificationForMessage({
-					roomId,
-					type,
-					content: message,
-					userId,
-				}) // send push notification
+				try {
+					await sendPushNotificationForMessage({
+						roomId,
+						type,
+						content: message,
+						userId,
+					}) // send push notification
+				} catch (error) {
+					console.log(error)
+				}
 
 				io.to(roomId).emit('receive_message', data)
 			} catch (error) {
