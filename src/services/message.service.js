546c3ede9cee661119eb6d49ee07b5b9b90ff518
mtri-git <vmtri20@gmail.com>
2023-07-07 18:19:16 +0700

    fix message service

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -16,9 +16,9 @@ const sendPushNotificationForMessage = async ({roomId, type, content, userId}) =
 	switch (type) {
 		case 'text':
 			messageContent =
-				message.content.length > 20
-					? message.content.slice(0, 20) + '...'
-					: message.content
+			content.content.length > 20
+					? content.slice(0, 20) + '...'
+					: content
 			break
 		case 'image':
 			messageContent = 'Đã gửi ảnh 📷'
