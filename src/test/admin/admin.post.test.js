082ba99bd1bafa6de56a59bf1726aefc4389cfec
mtri-git <vmtri20@gmail.com>
2023-07-16 09:39:49 +0700

    add more unit test

--- src/test/admin/admin.post.test.js
+++ src/test/admin/admin.post.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const app = require('../../app') // Đây là file chứa Express app của bạn
 
-describe('User API for admin', () => {
+describe('Post API for admin', () => {
 	let accessToken
 
 	it('should return a JWT token if the credentials are correct', async () => {
@@ -13,28 +13,28 @@ describe('User API for admin', () => {
 		// expect(response.body.token).toBeDefined()
 	})
 
-    let listUser = []
-	describe('GET user list /admin/user?offset=1', () => {
+    let listPost = []
+	describe('GET user list /admin/post?offset=1', () => {
 		it('Should return data contain list of user', async () => {
 			const response = await request(app)
 				.get('/api/v1/admin/user?offset=1')
 				.set('Authorization', `Bearer ${accessToken}`)
 				.expect(200)
-            listUser = response.body.data
+            listPost = response.body.data
 			expect(response.body.data).toBeDefined()
-            expect(listUser.length).toBeGreaterThan(0)
+            expect(listPost.length).toBeGreaterThan(0)
 		})
 	})
 
 	describe('Get User Detail', () => {
-		it('Should return data of user', async () => {
-            const userId = listUser[0]._id
+		it('Should return data of post', async () => {
+            const postId = listPost[0]._id
 			const response = await request(app)
-				.get(`/api/v1/admin/user/${userId}`)
+				.get(`/api/v1/admin/user/${postId}`)
 				.set('Authorization', `Bearer ${accessToken}`)
 				.expect(200)
             expect(response.body.data).toBeDefined()
-            expect(response.body.data._id).toBe(userId)
+            expect(response.body.data._id).toBe(postId)
 		})
 	})
 })
--- src/test/admin/admin.statistic.test.js
+++ src/test/admin/admin.statistic.test.js
@@ -10,34 +10,38 @@ describe('Statistic API', () => {
 			.send({ username: 'admin', password: '123' })
 			.expect(200)
 		accessToken = response.body.token
-		expect(response.body.token).toBeDefined()
+		// expect(response.body.token).toBeDefined()
 	})
 
-	it('Should return data contain total user, total room, total message', async () => {
-		const response = await request(app)
-			.get('/api/v1/admin/statistic/number')
-			.set('Authorization', `Bearer ${accessToken}`)
-			.expect(200)
-		expect(response.body.data.userNumber).toBeDefined()
-		expect(response.body.data.postNumber).toBeDefined()
-		expect(response.body.data.commentNumber).toBeDefined()
-		expect(response.body.data.chatNumber).toBeDefined()
-		expect(response.body.data.roomNumber).toBeDefined()
+	describe('GET /api/v1/admin/statistic/number', () => {
+		it('Should return data contain total user, total room, total message', async () => {
+			const response = await request(app)
+				.get('/api/v1/admin/statistic/number')
+				.set('Authorization', `Bearer ${accessToken}`)
+				.expect(200)
+			expect(response.body.data.userNumber).toBeDefined()
+			expect(response.body.data.postNumber).toBeDefined()
+			expect(response.body.data.commentNumber).toBeDefined()
+			expect(response.body.data.chatNumber).toBeDefined()
+			expect(response.body.data.roomNumber).toBeDefined()
+		})
 	})
 
-	it('Should return data of user join by month', async () => {
-		const response = await request(app)
-			.get('/api/v1/statistic/user-join-by-mouth')
-			.set('Authorization', `Bearer ${accessToken}`)
-			.expect(200)
-		expect(response.body.data).toBeDefined()
+	describe('Get User join by month', () => {
+		it('Should return data of user join by month', async () => {
+			const response = await request(app)
+				.get('/api/v1/statistic/user-join-by-mouth')
+				.set('Authorization', `Bearer ${accessToken}`)
+				.expect(200)
+		})
 	})
 
-	it('Should return data of user by age', async () => {
-		const response = await request(app)
-			.get('/api/v1/statistic/user-by-age')
-			.set('Authorization', `Bearer ${accessToken}`)
-			.expect(200)
-		expect(response.body.data).toBeDefined()
+	describe('Group User by age', () => {
+		it('Should return data of user by age', async () => {
+			const response = await request(app)
+				.get('/api/v1/statistic/user-by-age')
+				.set('Authorization', `Bearer ${accessToken}`)
+				.expect(200)
+		})
 	})
 })
